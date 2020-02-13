4492a1c2bb3dc20af69ff9ef115849a65410103f
andre <xplay568@gmail.com>
2020-02-12 23:07:34 -0600

    Solo falta cambios diagonales
    matriz de 10x10 ahora. Evalua diagonalmente.

--- Othelo.js
+++ Othelo.js
@@ -5,26 +5,28 @@ const isOver =false;
 //const root = document.getElementById('root');
 const horizontalSpaces = 8;
 const veritaclSpaces =8;
+//sera una matriz de 10 por 10 pero solo se renderizan las 8 casillas centrales
 const state= [
-	 [0,0,0,0,0,0,0,0,]
-	,[0,0,0,0,0,0,0,0,]
-	,[0,0,0,0,0,0,0,0,]
-	,[0,0,0,1,-1,0,0,0,]
-	,[0,0,0,-1,1,0,0,0,]
-	,[0,0,0,0,0,0,0,0,]
-	,[0,0,0,0,0,0,0,0,]
-	,[0,0,0,0,0,0,0,0,]];
+	 [0,0,0,0,0,0,0,0,0,0]
+	,[0,0,0,0,0,0,0,0,0,0]
+	,[0,0,0,0,0,0,0,0,0,0]
+	,[0,0,0,0,0,0,0,0,0,0]
+	,[0,0,0,0,1,-1,0,0,0,0]
+	,[0,0,0,0,-1,1,0,0,0,0]
+	,[0,0,0,0,0,0,0,0,0,0]
+	,[0,0,0,0,0,0,0,0,0,0]
+	,[0,0,0,0,0,0,0,0,0,0]
+	,[0,0,0,0,0,0,0,0,0,0]];
 	
 
 
-setTimeout(function(){}, 500);
+
 function clearBoard(){
 	const mount = document.getElementById("root");
     while (mount.firstChild) {
     mount.firstChild.remove();
 }
 }
-
 
 
 
@@ -39,63 +41,30 @@ board.style.width= '600px';
 board.style.height = '600px';
 //board.style.padding='1px';
 
-	for (let i=0;i<8;i++){
+	for (let i=1;i<9;i++){
 		row= document.createElement('div');
 		row.style.display = 'inline-block';
 		//row.style.padding='1px';
-		for(let j=0;j<8;j++){
+		for(let j=1;j<9;j++){
 			row.appendChild(createASquare(state[i][j],j,i));
 		}
 		board.appendChild(row);
 	}
+const status = document.createElement('div');
+status.style.width ="600px";
+status.style.height="30px";
+status.style.textAlign="center";
+status.style.fontSize="25px";
+if (isP1Turn) {status.textContent = "Turno de las Blancas";}else{status.textContent = "Turno de las Negras";}
 
+mount.appendChild(status);
 mount.appendChild(board);
 }
 
 
 
-
-
 renderBoard();
 
-// cyclo para jugar hasta que haya un ganador
-//es cpu vs cpu para ver el comportamiento
-// while (isThereAWinner()==false){
-// 	renderBoard();
-
-// 	 if (isP1Turn==true) {
-// 		console.log("it is turn of player 1");
-// 		//var tryAgain=true;
-// 		// while(tryAgain=true){
-// 			typeOfChip=1;
-// 			row=Math.floor(Math.random() * 8);
-// 			column=Math.floor(Math.random() * 8);
-// 			// if(isTurnValid(row,column)){
-// 			// 	
-// 			// 	}
-// 			makeAMove(row,column,typeOfChip);
-// 			// 	tryAgain=false;
-// 			//}	
-// 			isP1Turn=false;
-// 	} 
-// 	renderBoard();
-// 	if (isP1Turn==false){
-// 		console.log("it is turn of player 2");
-// 		//tryAgain=true;
-// 		//while(tryAgain=true){
-// 			row=Math.floor(Math.random() * 8);
-// 			column=Math.floor(Math.random() * 8);
-// 			//if(isTurnValid(row,column)){
-// 				makeAMove(row,column,-1);
-// 				//tryAgain=false;
-// 				isP1Turn=true;
-
-			
-// 		}	
-
-		
-
-// 	}
 
 function createASquare(value,row,column){
 	thisSquare = document.createElement('div');
@@ -121,9 +90,7 @@ function createASquare(value,row,column){
 	thisSquare.onclick = ()=> {
 		if(isTurnValid(row,column)){
 		if (isP1Turn) {makeAMove(row,column,-1);} else{makeAMove(row,column,1);} 
-		alert("me han presionado \n" + "mi fila es "+row + "\n mi columna es "+ column);
-	
-		// makeAMove(row,column,isP1Turn? -1: 1);
+		//alert("me han presionado \n" + "mi fila es "+row + "\n mi columna es "+ column);
 		}else {
 			alert("Invalid Move");
 		}
@@ -139,35 +106,14 @@ function makeAMove(column,row,typeOfChip){
 	state[row][column]=typeOfChip;
 	//printBonito();
 	refreshState(row, column, typeOfChip);
-	
-	renderBoard();
 	if (isP1Turn) {isP1Turn=false;}else{isP1Turn=true;}
+	renderBoard();
+	if(isThereAWinner()){
+		setTimeout(function(){
+			alert("Gracias por jugar");
+		}, 2000);
+	}
 	// printBonito();
-}
-
-
-function printBonito(){
-	for(i=0;i<8;i++){
-		console.log(state[i][0],state[i][1],state[i][2],state[i][3],state[i][4],state[i][5],state[i][6],state[i][7])
-	}
-	console.log();
-}
-
-
-
-function printState(){
-	for (let i =0; i<8; i++) {
-		for (let j=0; j<8; j++){
-			printASpace(state[i][j]);
-		}
-	}
-
-}
-
-
-
-function printTurn(){
-
 }
 
 function refreshState(row, column, typeOfChip){
@@ -187,7 +133,7 @@ function checkVertically(row, column, typeOfChip){
 		}
 	}
 	//revisa las casillas de la derecha en este sentido <-
-	for(b=7;b>row ; b--){
+	for(b=8;b>row ; b--){
 		
 		if(state[b][column]== typeOfChip){
 			for(n=b;n>row; n--){
@@ -208,7 +154,7 @@ function checkHorizontally(row,column, typeOfChip) {
 		}
 	}
 	//revisa las casillas de la derecha en este sentido <-
-	for(b=7;b>column ; b--){
+	for(b=8;b>column ; b--){
 		
 		if(state[row][b]== typeOfChip){
 			for(n=b;n>column; n--){
@@ -221,23 +167,28 @@ function checkHorizontally(row,column, typeOfChip) {
 
 
 
-
-
-
-
 function checkDiagonally(row,column,typeOfChip){}
 
 function isTurnValid(column,row){
-	if (state[row][column]==0) {return true;} else{return false;}
-	//return (state[row][column]==0)? (true) : (false);
+	if (state[row][column]==0) {
+		//casos para el turno de -1
+		if (isP1Turn) { opposite= 1; } else {opposite=-1;}
+		
+		if(state[row+1][column]==opposite||state[row-1][column]==opposite
+			||state[row][column+1]==opposite||state[row][column-1]==opposite
+			||state[row+1][column+1]==opposite||state[row-1][column-1]==opposite
+			||state[row-1][column+1]==opposite||state[row+1][column-1]==opposite){
+					return true;
+				}
+			}else{
+				return false;}
+
 
 }
 
 function isThereAWinner(){
-	for (i =0; i<8; i++){
-		for (j =0; j<8; j++){
-			// if (state[i][j]==1){amountOfBlackChips++; console.log("Negras: "+ amountOfBlackChips);}
-			// if (state[i][j]==-1){amountOfWhiteChips++; console.log("Blancas: "+ amountOfBlackChips);}
+	for (i =1; i<9; i++){
+		for (j =1; j<9; j++){
 			if (state[i][j]==0){return false;}
 	}
 	finalCount();
@@ -249,12 +200,16 @@ function isThereAWinner(){
 function finalCount(){
 	amountOfWhiteChips=0;
 	amountOfBlackChips=0;
-	for (i =0; i<8; i++){
-		for (j =0; j<8; j++){
+	for (i =1; i<9; i++){
+		for (j =1; j<9; j++){
 			if (state[i][j]==1){amountOfBlackChips++;}
 			if (state[i][j]==-1){amountOfWhiteChips++; }
 			
 }}
- console.log("Negras: "+ amountOfBlackChips);
- console.log("Blancas: "+ amountOfBlackChips);
+ setTimeout(function(){
+			alert("HAY UN GANADOOR");
+			alert("Blancas: "+ amountOfBlackChips);
+			alert("Negras: "+ amountOfBlackChips);
+		}, 1000);
+ 
 }
